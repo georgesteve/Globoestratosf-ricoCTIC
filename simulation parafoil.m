6f77be4b1a8037b065e5bebcc168e456b7d4f7df
georgesteve <37008877+georgesteve@users.noreply.github.com>
2019-02-18 21:59:09 -0500

    Create simulation parafoil.m

---
+++ simulation parafoil.m
@@ -0,0 +1,24 @@
+#Christiaan Redelinghuys∗
+
+sym t p y
+
+
+
+VH1l=[sin(t) cos xLH  cos sin  sin sin coszLH cos sin  xLH  cos cos  cos zLH sin cos  cos sin sinzLH
+cos cos  xLH  sin sin  cos sin  coszLH sin sin  xLH  sin cos  cos zLH cos cos  sin sin  sinzLH
+0 cos xLH  sin  cos zLH cos  sin zLH]
+
+cos  sin  cos rzpH  cos  cos rzpH 0  cos rzpH 0  sin r
+sin  cos r  cos  cos  sin rzpH sin  sin rzpH cos rzpH 0 00
+cos  sin  sin rzpH cos  sin rzpH 0 sin rzpH 0  cos r
+
+
+ILxx sin   ILxz cos  cos  ILxz sin  ILxx
+ILyy cos  sin  ILyy cos  0
+ILzz cos  cos   ILxz sin  ILzz sin  ILxz
+
+Ipxxp1; 1  Ipxzp1;3 Ipxxp2;1  Ipxzp2;3 Ipxxp3;1  Ipxzp3;3 Ipxxpr1;1  Ipxzpr1;3 Ipxxpr2;1  Ipxzpr2;3
+Ipyyp1;2 Ipyyp2;2 Ipyyp3;2 Ipyypr1;2 Ipyypr2;2
+Ipzzp1; 3  Ipxzp1;1 Ipzzp2;3  Ipxzp2;1 Ipzzp3;3  Ipxzp3;1 Ipzzpr1;3  Ipxzpr1;1 Ipzzpr2;3  Ipxzpr2;1
+
+
